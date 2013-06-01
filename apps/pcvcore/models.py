c07458505ffa65ed4012867ea720dc785e102a21
Sean Abbott <sean.abbott@gmail.com>
2013-06-01 18:14:49 -0400

    started on pcvcore/models

--- apps/pcvcore/models.py
+++ apps/pcvcore/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.worldmap import data_options
 
 # Create your models here.
+class PCVProfile(models.Model):
+    country = models.CharField(choice=data_options.COUNTRIES)
+    sector = models.CharField(choices=data_options.SECTORS)
+    startDate = models.DateField()
+    endDate = models.DateField()
+
+class Teacher(models.Model):
+    school = models.CharField(max_length=128)
+    grade = models.CharField(max_length=5)
+    
