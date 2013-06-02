40e69d9bb4eaa378f0d9425da118f06eae154ea0
Ethan Soergel <esoergel@gmail.com>
2013-06-02 13:58:27 -0400

    minor changes

--- apps/worldmap/views.py
+++ apps/worldmap/views.py
@@ -26,6 +26,9 @@ class MapView(TemplateView):
 
 
 class BlogJSON(JSONResponseMixin, View):
+    """
+    filter options:
+    """
 
     def get(self, request, *args, **kwargs):
         context = {
