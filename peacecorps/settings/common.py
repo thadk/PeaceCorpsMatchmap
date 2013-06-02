c9eef9495b38c807f15fab3de86f79e1e4a26d3c
Ethan Soergel <esoergel@gmail.com>
2013-06-02 11:26:31 -0400

    saving before merge

--- peacecorps/settings/common.py
+++ peacecorps/settings/common.py
@@ -115,7 +115,6 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'data',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
