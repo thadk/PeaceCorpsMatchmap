aec2b7bc990ed354afa4409d07544f143b2253ca
Ethan Soergel <esoergel@gmail.com>
2013-06-02 12:53:26 -0400

    added some logic to login page

--- apps/pcvcore/views.py
+++ apps/pcvcore/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+
+
+class DebugMixin(object):
+    """
+    For use in testing out a new view
+    """
+    def get_context_data(self, **kwargs):
+        "This is to print your context variables during testing ONLY"
+        context = super(DebugMixin, self).get_context_data(**kwargs)
+        print "#########################"
+        print "CONTEXT SENT TO TEMPLATE:"
+        for k, v in context.items():
+            print "  %s: %s" % (k, v)
+        print "DON'T FORGET TO REMOVE THIS MIXIN AFTER TESTING THE VIEW"
+        print "#########################"        
+        return context
