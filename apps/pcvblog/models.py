9eac8a917376a98b5a4f234fb517a7d5aa70b99e
Seth Madison <seth.madison@gmail.com>
2013-06-02 13:42:15 -0400

    First commit of blog stuff with list and creation. Still TODO:
     * Permissions
     * Images
     * Styling
     * TinyMCE/Markdown for posts
     * Confirm for Delete

--- apps/pcvblog/models.py
+++ apps/pcvblog/models.py
@@ -1,9 +1,24 @@
+from django.template.defaultfilters import slugify
+from django.core.urlresolvers import reverse
 from django.db import models
 
 class Entry(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="blog-images")
+    image = models.ImageField(upload_to="blog-images", null=True, blank=True)
+    slug = models.SlugField(db_index=True)
     body = models.TextField()
     post_time = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def abstract(self):
+        return self.body[:30] + "..."
+
+    def save(self, *args, **kwargs):
+        ## TODO: Should slug be unique? If so we need db constraint
+        #        and a check here or in ``clean``
+        self.slug = slugify(self.title)
+        super(Entry, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('entry_list')
--- peacecorps/urls.py
+++ peacecorps/urls.py
@@ -24,4 +24,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^$','data.views.home',name='home'),
     # url(r'^register/$', 'data.views.register', name="register")
+    url(r'^blog/', include('apps.pcvblog.urls')),
 )
