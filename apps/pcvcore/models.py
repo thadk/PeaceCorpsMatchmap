03645090a8fd70e4fa916ffcfd24bcfd1969b38f
Sean Abbott <sean.abbott@gmail.com>
2013-06-02 11:11:59 -0400

    fixed model eel errors

--- apps/pcvcore/models.py
+++ apps/pcvcore/models.py
@@ -3,8 +3,8 @@ from apps.worldmap import data_options
 
 # Create your models here.
 class PCVProfile(models.Model):
-    country = models.CharField(choice=data_options.COUNTRIES)
-    sector = models.CharField(choices=data_options.SECTORS)
+    country = models.CharField(choices=data_options.COUNTRIES, max_length=128)
+    sector = models.CharField(choices=data_options.SECTORS, max_length=128)
     start_date = models.DateField()
     end_date = models.DateField()
 
@@ -15,12 +15,12 @@ class Teacher(models.Model):
 
 class School(models.Model):
     city = models.CharField(max_length=128)
-    state = models.CharField(choices=data_options.STATES)
+    state = models.CharField(choices=data_options.STATES, max_length=128)
     school_name = models.CharField(max_length=128)
     zip_code = models.CharField(max_length=10)
-    about = models.CharField()
+    about = models.CharField(max_length=2048)
 
 class UserProfile(models.Model):
     is_pcv = models.BooleanField()
-    address = models.CharField()
-    bio = modles.CharField()
+    address = models.CharField(max_length=128)
+    bio = models.CharField(max_length=2048)
