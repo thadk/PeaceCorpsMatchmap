46477e874d42c3d46bd967051d1c79099544e2d7
Sean Abbott <sean.abbott@gmail.com>
2013-06-02 10:58:37 -0400

    finished pcvcore model

--- apps/pcvcore/models.py
+++ apps/pcvcore/models.py
@@ -5,10 +5,22 @@ from apps.worldmap import data_options
 class PCVProfile(models.Model):
     country = models.CharField(choice=data_options.COUNTRIES)
     sector = models.CharField(choices=data_options.SECTORS)
-    startDate = models.DateField()
-    endDate = models.DateField()
+    start_date = models.DateField()
+    end_date = models.DateField()
 
 class Teacher(models.Model):
     school = models.CharField(max_length=128)
     grade = models.CharField(max_length=5)
-    
+    following = models.ForeignKey('auth.User')
+
+class School(models.Model):
+    city = models.CharField(max_length=128)
+    state = models.CharField(choices=data_options.STATES)
+    school_name = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=10)
+    about = models.CharField()
+
+class UserProfile(models.Model):
+    is_pcv = models.BooleanField()
+    address = models.CharField()
+    bio = modles.CharField()
