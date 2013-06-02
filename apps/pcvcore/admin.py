4c49ea9ba9b51dc006074f2418efb328f3bcf3e6
Ethan Soergel <esoergel@gmail.com>
2013-06-02 15:19:34 -0400

    AJAX view now works

--- apps/pcvcore/admin.py
+++ apps/pcvcore/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import PCVProfile, Teacher, School, UserProfile
+from .models import PCVProfile, Teacher, School
 
 admin.site.register(PCVProfile)
 admin.site.register(Teacher)
 admin.site.register(School)
-admin.site.register(UserProfile)
