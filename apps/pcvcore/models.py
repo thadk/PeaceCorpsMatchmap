8ce1894d14b4cd4a4afae13cc5fe0f5e309b20b8
Ethan Soergel <esoergel@TheGibson.(none)>
2013-06-04 21:59:10 -0400

    separated out AJAX stuff into a mixin, and made filtering work

--- apps/pcvcore/models.py
+++ apps/pcvcore/models.py
@@ -4,11 +4,12 @@ from apps.worldmap import data_options
 class PCVProfile(models.Model):
     is_pcv = models.BooleanField(default=True)
     user = models.OneToOneField('auth.User')
-    country = models.CharField(choices=data_options.COUNTRIES, max_length=128, blank=True)
+    country = models.CharField("Post Country", choices=data_options.COUNTRIES, max_length=128, blank=True)
     sector = models.CharField(choices=data_options.SECTORS, max_length=128, blank=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    address = models.CharField(max_length=128, blank=True)
+    home_address = models.CharField(max_length=128, blank=True, default="")
+    home_state = models.CharField(choices=data_options.STATES, max_length=128, blank=True, default="")
     bio = models.TextField(blank=True)
     def __unicode__(self):
         return self.user.username
