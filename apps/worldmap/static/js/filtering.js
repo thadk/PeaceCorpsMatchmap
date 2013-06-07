e25c8b0a689129be1912f04a10db59ed0b9c4006
beechnut <cloydster@gmail.com>
2013-06-07 14:05:57 -0400

    Fix multiparameter querying and add fixtures

--- apps/worldmap/static/js/filtering.js
+++ apps/worldmap/static/js/filtering.js
@@ -7,12 +7,15 @@
 $(function() {
 
   $("form").change(function() {
-    var str = $("form").serialize();
+    var str = $('form').serialize();
+    str = str.replace(/[^&]+=\.?(?:&|$)/g, '')
+    //str = "gradelevel=4";
     alert(str); // Just for debugging purposes right now.
 
     // Add posts into sidebar
-    $.getJSON('map/get_blogs', function(data) {   // add { query: str } once working
+    $.getJSON('map/get_blogs/?' + str, function(data) {
       $('#sidebar').empty();
+      // TODO: Handle empty set.
       $.each(data["posts"], function(key, val){
           $('#sidebar').append("<h2>" + val["title"] + "</h2>");
           $('#sidebar').append(val["text"]).addClass("sidebar-post");
