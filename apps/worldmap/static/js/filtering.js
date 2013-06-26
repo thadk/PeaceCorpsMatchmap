2b87714246b34761567ba4c392e8c588f943043d
beechnut <cloydster@gmail.com>
2013-06-25 20:14:02 -0400

    Fix filtering.js based on new JSON

--- apps/worldmap/static/js/filtering.js
+++ apps/worldmap/static/js/filtering.js
@@ -23,8 +23,8 @@ $(function() {  // On document ready
   // Truncate the post body text
 
   $.postpreview = function(post) {
-    var p = post["text"];
-    var preview = p.substr(0, 150) + "...";
+    var body = post["body"];
+    var preview = body.substr(0, 150) + "...";
     preview = preview + "<a onclick=''>Read more</a>";
     return preview;
   }
@@ -33,12 +33,12 @@ $(function() {  // On document ready
   // Form partial
 
   $.postpartial = function(post) {
-    var name = post["user"]["username"]
-    var title = post["title"]
+    var name = post.author.username;
+    var title = post.title;
 
     var partial = "<br/><br/><div class='sidebar-post'>";
     partial = partial + "<span class='post-title'>" + title + "</span><br/>";
-    partial = partial + "<span class='post-byline'>by <a href='" + name + "'>" + name + "</a> on " + $.date(post["date"]) + "</span><br/>";
+    partial = partial + "<span class='post-byline'>by <a href='" + name + "'>" + name + "</a> on " + $.date(post["post_time"]) + "</span><br/>";
     partial = partial + "<span class='post-body'>" + $.postpreview(post) + "</span>";
     return partial;
   }
@@ -53,41 +53,39 @@ $(function() {  // On document ready
     // Add posts into sidebar
     $.getJSON('map/get_blogs/?' + str, function(data) {
       $('#sidebar').empty();
-      if (data["posts"].length == 0){
+      if (data["objects"].length == 0){
         $('#sidebar').append("<br/><br/>No blog entries matched your search. Clear the search and try again.");
-      }b
-      // TODO: Handle country or profile header object.
-      if (data["country"]) {
-        alert("Got header!:" + data["country"]);
-        var country = data["country"];
-        var zoomlevel = country["zoomlevel"];
-        var lng = country["coords"]["lng"];
-        var lat = country["coords"]["lat"];
-        map.zoom(zoomlevel).center({ lon: lng, lat: lat });
       }
+      // TODO: Handle country or profile header object.
+      // if (data["country"]) {
+      //   alert("Got header!:" + data["country"]);
+      //   var country = data["country"];
+      //   var zoomlevel = country["zoomlevel"];
+      //   var lng = country["coords"]["lng"];
+      //   var lat = country["coords"]["lat"];
+      //   map.zoom(zoomlevel).center({ lon: lng, lat: lat });
+      // }
       // Display in sidebar a partial for each post
-      $.each(data["posts"], function(key, post){
-          $('#sidebar').append($.postpartial(post));
+      $.each(data["objects"], function(index,post){
+        $('#sidebar').append($.postpartial(post));
       });
     });
+  }
 
-    $.initializeMap = function(){
-      // mapbox.auto('map', 'matchmap.map-7egeornz');
-      var map = mapbox.map('map');
-      map.addLayer(mapbox.layer().id('matchmap.map-7egeornz'));
-      map.zoom(6).center({lon: 42, lat: -71});
-    }
+  $.updateMap = function(coords){
+    lng = coords[0]
+    lat = coords[1]
+    map.center({ lon: lng, lat: lat });
   }
 
   // Update posts on page load
-
-  $.initializeMap();
   $.updatePosts();
 
   // Update posts when a field is changed or search box is submitted
 
   $("form").change(function() {
     $.updatePosts();
+    $.updateMap([20, 20]);
   });
 
   $("form").submit(function(event) {
