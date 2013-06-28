ba9331bc88245d7f2a77b86295c6a439e21b6a4b
Ethan Soergel <esoergel@gmail.com>
2013-06-28 14:14:21 -0400

    made server not break if you don't have environ variables set

--- peacecorps/settings/production.py
+++ peacecorps/settings/production.py
@@ -20,8 +20,8 @@ ALLOWED_HOSTS = ['peacecorps.herokuapp.com']
 #####################
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME', "")
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', "")
 EMAIL_PORT = 587        # 25, 587, 2525 and 465 on ssl
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'thadknull@gmail.com'
